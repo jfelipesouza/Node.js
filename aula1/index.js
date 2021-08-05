cc5c797a5f6992bf5614697a5edc783eb4af81b3
Felipe Souza <j.felipe.dev.18@gmil.com>
2021-08-05 11:48:07 -0300

    Finalisando a aula 1

---
+++ aula1/index.js
@@ -0,0 +1,11 @@
+const http = require('http'); // Esta importando o modulo http
+
+http.createServer((req,res)=>{ // Função de callback que recebe informações da porta do servidor e envia respostas
+    res.writeHead(200,{ // Si ok escreva um texto. Porta 200 é a de tudo ok
+        "Contentent-Type":"text/plain"
+    })
+
+    res.write('Felipe eh incrivel') // Leia sendo "Escreva('Oque esta escrito aqui')"
+    res.end();// Finalisando o servidor 
+
+}).listen(1500)/*Porta do servidor*/;
